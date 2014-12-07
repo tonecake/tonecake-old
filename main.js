cb1a94033f91f2b2fa778d56b257ca2a6b11095b
cholalo <violinist.ch@gmail.com>
2014-12-07 22:32:30 +0100

    인터페이스 개선 시작

---
+++ main.js
@@ -0,0 +1,11 @@
+// javascript document for tonecake
+
+var menu = {
+
+    create : function(o){
+
+        var dom = {
+            container : document.querySelector('.menu'),
+        };
+    }
+}
